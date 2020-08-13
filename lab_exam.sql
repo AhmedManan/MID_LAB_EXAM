f5d56a3f2489612033073983777bd998cb1604ba
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2020-08-13 13:34:08 +0600

    Add files via upload

---
+++ lab_exam.sql
@@ -0,0 +1,456 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 13, 2020 at 09:33 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lab_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `block`
+--
+
+CREATE TABLE `block` (
+  `id` int(10) NOT NULL,
+  `sender` int(10) NOT NULL,
+  `receiver` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(10) NOT NULL,
+  `posterid` varchar(10) NOT NULL,
+  `post` int(10) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend`
+--
+
+CREATE TABLE `friend` (
+  `id` int(10) NOT NULL,
+  `personid` int(10) NOT NULL,
+  `friendid` int(10) NOT NULL,
+  `follow` tinyint(1) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `like`
+--
+
+CREATE TABLE `like` (
+  `id` int(10) NOT NULL,
+  `posterid` varchar(10) NOT NULL,
+  `postid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_cred`
+--
+
+CREATE TABLE `login_cred` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `pid` int(10) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `type` varchar(6) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `dob` date NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  `action_field` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login_cred`
+--
+
+INSERT INTO `login_cred` (`id`, `name`, `pid`, `username`, `email`, `password`, `type`, `phone`, `gender`, `dob`, `designation`, `action_field`) VALUES
+(2, 'Manan Ahmed', 5, 'manan', 'ahmed.mnn@outlook.com', '123', 'admin', '01748868061', 'male', '0000-00-00', 'Supervisor', 'Field Worker'),
+(3, 'Jakir Khan', 0, 'jakir', 'jakir@gmail.com', '1234', 'member', '01748868000', 'male', '0000-00-00', 'none', 'Field Worker');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(10) NOT NULL,
+  `senderid` int(10) NOT NULL,
+  `reciveid` int(10) NOT NULL,
+  `description` varchar(5000) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp(),
+  `seen` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(10) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp(),
+  `seen` tinyint(1) NOT NULL,
+  `receiverid` varchar(10) NOT NULL,
+  `senderid` varchar(10) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `link` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page`
+--
+
+CREATE TABLE `page` (
+  `pageid` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `displayimg` varchar(100) NOT NULL,
+  `backgroundimg` varchar(100) NOT NULL,
+  `creationdate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pagefollower`
+--
+
+CREATE TABLE `pagefollower` (
+  `id` int(10) NOT NULL,
+  `pageid` int(10) NOT NULL,
+  `personid` int(10) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_admin`
+--
+
+CREATE TABLE `page_admin` (
+  `id` int(10) NOT NULL,
+  `pageid` int(10) NOT NULL,
+  `personid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_editor`
+--
+
+CREATE TABLE `page_editor` (
+  `id` int(10) NOT NULL,
+  `pageid` int(10) NOT NULL,
+  `personid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_info`
+--
+
+CREATE TABLE `personal_info` (
+  `id` int(10) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `dob` date NOT NULL,
+  `school` text DEFAULT NULL,
+  `college` text DEFAULT NULL,
+  `university` text DEFAULT NULL,
+  `displayimg` varchar(100) DEFAULT NULL,
+  `backgroundimg` varchar(100) DEFAULT NULL,
+  `joiningdate` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(10) NOT NULL,
+  `posterid` varchar(10) NOT NULL,
+  `description` varchar(10000) NOT NULL,
+  `media` varchar(500) DEFAULT NULL,
+  `privacy` varchar(7) NOT NULL,
+  `time` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `id` int(10) NOT NULL,
+  `reportedid` varchar(10) NOT NULL,
+  `reporterid` varchar(10) NOT NULL,
+  `reason` varchar(100) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp(),
+  `type` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `share`
+--
+
+CREATE TABLE `share` (
+  `id` int(10) NOT NULL,
+  `personid` int(10) NOT NULL,
+  `postid` int(10) NOT NULL,
+  `description` varchar(10000) DEFAULT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `block`
+--
+ALTER TABLE `block`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `friend`
+--
+ALTER TABLE `friend`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `like`
+--
+ALTER TABLE `like`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_cred`
+--
+ALTER TABLE `login_cred`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pid` (`pid`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `page`
+--
+ALTER TABLE `page`
+  ADD PRIMARY KEY (`pageid`);
+
+--
+-- Indexes for table `pagefollower`
+--
+ALTER TABLE `pagefollower`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `page_admin`
+--
+ALTER TABLE `page_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `page_editor`
+--
+ALTER TABLE `page_editor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `share`
+--
+ALTER TABLE `share`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `block`
+--
+ALTER TABLE `block`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `friend`
+--
+ALTER TABLE `friend`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `like`
+--
+ALTER TABLE `like`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `login_cred`
+--
+ALTER TABLE `login_cred`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `page`
+--
+ALTER TABLE `page`
+  MODIFY `pageid` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pagefollower`
+--
+ALTER TABLE `pagefollower`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `page_admin`
+--
+ALTER TABLE `page_admin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `page_editor`
+--
+ALTER TABLE `page_editor`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `share`
+--
+ALTER TABLE `share`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  ADD CONSTRAINT `ID` FOREIGN KEY (`id`) REFERENCES `login_cred` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
